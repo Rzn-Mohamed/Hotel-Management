0f68db27bd09a922af010a09ed8c412b49ba3180
Anasjrr <anaskacm89@gmail.com>
2025-01-01 18:27:56 +0100

    AddContactUs_model

---
+++ Models/ContactUs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Management.Models
+{
+    internal class ContactUs
+    {
+        public int Id { get; set; }
+
+        public String FirstName { get; set; } 
+        public String LastName { get; set; }
+        public String Email { get; set; } 
+        public String Message { get; set; } 
+
+    }
+}
